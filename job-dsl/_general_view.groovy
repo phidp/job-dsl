f160b920c8d1eb56a5d13679bc672b4eb983fb34
phidp <phidp@techcombank.com.vn>
2023-12-13 09:55:34 +0700

    init

--- job-dsl/_general_view.groovy
+++ job-dsl/_general_view.groovy
@@ -6,9 +6,9 @@ def dir = new File("${workDir}/job-dsl")
 def rootPath = dir.path
 
 dir.eachFileRecurse (FileType.DIRECTORIES) { fol ->
-
     def relativePath = fol.path.replace(rootPath + '/', '')
-    def formatedPath = relativePath.toLowerCase().replaceAll(' ', '-')
+    def formatedPath = relativePath.toLowerCase().replaceAll(' ', '-').replaceAll('_', '-')
+    def formatedName = formatInput(fol.name)
     println("Creating folder: ${relativePath}")
     println("Creating folder: ${formatedPath}")
 
@@ -16,4 +16,20 @@ dir.eachFileRecurse (FileType.DIRECTORIES) { fol ->
         displayName(fol.name)
         description(fol.name)
     }
+}
+
+def formatInput(String input) {
+    // Replace underscores with spaces
+    String replaced = input.replace('_', ' ')
+
+    // Split the string into words
+    List<String> words = replaced.split(' ')
+
+    // Capitalize each word
+    List<String> capitalizedWords = words.collect { it.capitalize() }
+
+    // Join words back together
+    String formattedInput = capitalizedWords.join(' ')
+
+    return formattedInput
 }
